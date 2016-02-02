f08e51e762d7fa52625361fb35a29fed098ee344
Federico <federico.fiorini29@gmail.com>
2016-02-02 16:43:20 +0100

    remove comments

--- src/main/python/mysql-queries.py
+++ src/main/python/mysql-queries.py
@@ -219,238 +219,238 @@ def dropDijkstra():
 	runIsolated(sql)
 
 # Query 1
-# print "================================================"
-# print "Query #1: Find the most frequent route per month"
+print "================================================"
+print "Query #1: Find the most frequent route per month"
 
-# sql = """
-# 	SELECT SQL_NO_CACHE r2.year, r2.month, r2.origin, a1.city AS origin_city, a1.state  AS origin_state, r2.destination, a2.city AS destination_city, a2.state  AS destination_state, max(r2.monthly_freq) AS max_monthly_freq
-# 	FROM (SELECT r.year, r.month, r.origin, r.destination, sum(r.frequency) AS monthly_freq
-# 		FROM flights.routes as r
-# 		GROUP BY r.year, r.month, r.origin, r.destination
-# 		ORDER BY sum(r.frequency) DESC) as r2
-# 	JOIN flights.airports as a1
-# 		ON r2.origin = a1.code
-# 	JOIN flights.airports as a2
-# 		ON r2.destination = a2.code
-# 	GROUP BY r2.year, r2.month;
-# 	"""
-# (results, timeList, time) = runQueryAndGetTime(sql)
+sql = """
+	SELECT SQL_NO_CACHE r2.year, r2.month, r2.origin, a1.city AS origin_city, a1.state  AS origin_state, r2.destination, a2.city AS destination_city, a2.state  AS destination_state, max(r2.monthly_freq) AS max_monthly_freq
+	FROM (SELECT r.year, r.month, r.origin, r.destination, sum(r.frequency) AS monthly_freq
+		FROM flights.routes as r
+		GROUP BY r.year, r.month, r.origin, r.destination
+		ORDER BY sum(r.frequency) DESC) as r2
+	JOIN flights.airports as a1
+		ON r2.origin = a1.code
+	JOIN flights.airports as a2
+		ON r2.destination = a2.code
+	GROUP BY r2.year, r2.month;
+	"""
+(results, timeList, time) = runQueryAndGetTime(sql)
 
-# table = PrettyTable(["year", "month", "origin", "origin_city", "origin_state", "destination", "destination_city", "destination_state", "max_monthly_freq"])
-# for row in results:
-# 	table.add_row(row)
-# print table
-# print map(str, timeList)
-# print "Time: " + str(time) + "\n"
+table = PrettyTable(["year", "month", "origin", "origin_city", "origin_state", "destination", "destination_city", "destination_state", "max_monthly_freq"])
+for row in results:
+	table.add_row(row)
+print table
+print map(str, timeList)
+print "Time: " + str(time) + "\n"
 
-# # Query 1.b
-# print "================================================="
-# print "Query #1.b: Find the most frequent route per year"
+# Query 1.b
+print "================================================="
+print "Query #1.b: Find the most frequent route per year"
 
-# sql = """
-# 	SELECT SQL_NO_CACHE r2.year, r2.origin, a1.city AS origin_city, a1.state  AS origin_state, r2.destination, a2.city AS destnation_city, a2.state  AS destnation_state, max(r2.yearly_freq) AS max_yearly_freq
-# 	FROM (SELECT r.year, r.origin, r.destination, sum(r.frequency) AS yearly_freq
-# 		FROM flights.routes as r
-# 		GROUP BY r.year, r.origin, r.destination
-# 		ORDER BY sum(r.frequency) DESC) as r2
-# 	JOIN flights.airports as a1
-# 		ON r2.origin = a1.code
-# 	JOIN flights.airports as a2
-# 		ON r2.destination = a2.code
-# 	GROUP BY r2.year;
-# 	"""
-# (results, timeList, time) = runQueryAndGetTime(sql)
+sql = """
+	SELECT SQL_NO_CACHE r2.year, r2.origin, a1.city AS origin_city, a1.state  AS origin_state, r2.destination, a2.city AS destnation_city, a2.state  AS destnation_state, max(r2.yearly_freq) AS max_yearly_freq
+	FROM (SELECT r.year, r.origin, r.destination, sum(r.frequency) AS yearly_freq
+		FROM flights.routes as r
+		GROUP BY r.year, r.origin, r.destination
+		ORDER BY sum(r.frequency) DESC) as r2
+	JOIN flights.airports as a1
+		ON r2.origin = a1.code
+	JOIN flights.airports as a2
+		ON r2.destination = a2.code
+	GROUP BY r2.year;
+	"""
+(results, timeList, time) = runQueryAndGetTime(sql)
 
-# table = PrettyTable(["year", "origin", "origin_city", "origin_state", "destination", "destination_city", "destination_state", "max_yearly_freq"])
-# for row in results:
-# 	table.add_row(row)
-# print table
-# print map(str, timeList)
-# print "Time: " + str(time) + "\n"
-
-
-# # Query 2
-# print "======================================================================="
-# print "Query #2: Find the airport with more flights (in and out) per month"
-
-# sql = """
-# 	SELECT SQL_NO_CACHE year, month, airport, max(flights) AS tot_flights
-# 	FROM
-# 		(SELECT year, month, airport, sum(monthly_freq) AS flights
-# 		FROM
-# 			(
-# 				(SELECT r.year, r.month, r.origin AS airport, sum(r.frequency) AS monthly_freq
-# 				FROM flights.routes as r
-# 				GROUP BY r.year, r.month, r.origin)
-# 			UNION
-# 				(SELECT r.year, r.month, r.destination AS airport, sum(r.frequency) AS monthly_freq
-# 				FROM flights.routes as r
-# 				GROUP BY r.year, r.month, r.destination)
-# 			) as u
-# 		GROUP BY year, month, airport
-# 		ORDER BY sum(monthly_freq) DESC) as m
-# 	GROUP BY year, month;
-# 	"""
-# (results, timeList, time) = runQueryAndGetTime(sql)
-
-# table = PrettyTable(["year", "month", "airport", "tot_flights"])
-# for row in results:
-# 	table.add_row(row)
-# print table
-# print map(str, timeList)
-# print "Time: " + str(time) + "\n"
-
-# # Query 2.b
-# print "======================================================================="
-# print "Query #2.b: Find the airport with more flights (in and out) per year"
-
-# sql = """
-# 	SELECT SQL_NO_CACHE year, airport, max(flights) AS tot_flights
-# 	FROM 
-# 		(SELECT year, airport, sum(yearly_freq) AS flights
-# 		FROM
-# 			(
-# 				(SELECT r.year, r.origin AS airport, sum(r.frequency) AS yearly_freq
-# 				FROM flights.routes as r
-# 				GROUP BY r.year, r.origin)
-# 			UNION
-# 				(SELECT r.year, r.destination AS airport, sum(r.frequency) AS yearly_freq
-# 				FROM flights.routes as r
-# 				GROUP BY r.year, r.destination)
-# 			) as u
-# 		GROUP BY year, airport
-# 		ORDER BY sum(yearly_freq) DESC) as m
-# 	GROUP BY year;
-# 	"""
-# (results, timeList, time) = runQueryAndGetTime(sql)
-
-# table = PrettyTable(["year", "airport", "tot_flights"])
-# for row in results:
-# 	table.add_row(row)
-# print table
-# print map(str, timeList)
-# print "Time: " + str(time) + "\n"
+table = PrettyTable(["year", "origin", "origin_city", "origin_state", "destination", "destination_city", "destination_state", "max_yearly_freq"])
+for row in results:
+	table.add_row(row)
+print table
+print map(str, timeList)
+print "Time: " + str(time) + "\n"
 
 
-# # Query 3
-# print "========================================================================================"
-# print "Query #3: Given a departure date find all the shortest paths from airport A to airport B"
-# print "----------------------------------------------------------------------------------------"
+# Query 2
+print "======================================================================="
+print "Query #2: Find the airport with more flights (in and out) per month"
 
-# year = "2011"
-# month = "6"
-# day = "20"
-# departure = "RAP"
-# arrival = "XNA"
+sql = """
+	SELECT SQL_NO_CACHE year, month, airport, max(flights) AS tot_flights
+	FROM
+		(SELECT year, month, airport, sum(monthly_freq) AS flights
+		FROM
+			(
+				(SELECT r.year, r.month, r.origin AS airport, sum(r.frequency) AS monthly_freq
+				FROM flights.routes as r
+				GROUP BY r.year, r.month, r.origin)
+			UNION
+				(SELECT r.year, r.month, r.destination AS airport, sum(r.frequency) AS monthly_freq
+				FROM flights.routes as r
+				GROUP BY r.year, r.month, r.destination)
+			) as u
+		GROUP BY year, month, airport
+		ORDER BY sum(monthly_freq) DESC) as m
+	GROUP BY year, month;
+	"""
+(results, timeList, time) = runQueryAndGetTime(sql)
 
-# print "Route: %s -> %s" % (departure, arrival)
-# print "Departure: %s/%s/%s" % (day, month, year)
+table = PrettyTable(["year", "month", "airport", "tot_flights"])
+for row in results:
+	table.add_row(row)
+print table
+print map(str, timeList)
+print "Time: " + str(time) + "\n"
 
-# a = datetime.datetime.now()
+# Query 2.b
+print "======================================================================="
+print "Query #2.b: Find the airport with more flights (in and out) per year"
 
-# initDBforDijkstra()
+sql = """
+	SELECT SQL_NO_CACHE year, airport, max(flights) AS tot_flights
+	FROM 
+		(SELECT year, airport, sum(yearly_freq) AS flights
+		FROM
+			(
+				(SELECT r.year, r.origin AS airport, sum(r.frequency) AS yearly_freq
+				FROM flights.routes as r
+				GROUP BY r.year, r.origin)
+			UNION
+				(SELECT r.year, r.destination AS airport, sum(r.frequency) AS yearly_freq
+				FROM flights.routes as r
+				GROUP BY r.year, r.destination)
+			) as u
+		GROUP BY year, airport
+		ORDER BY sum(yearly_freq) DESC) as m
+	GROUP BY year;
+	"""
+(results, timeList, time) = runQueryAndGetTime(sql)
 
-# sql = """
-# 	SELECT SQL_NO_CACHE r.origin, r.destination
-# 	FROM flights.routes r
-# 	WHERE r.year = "%s" AND r.month = "%s" AND r.day = "%s";
-# 	""" % (year, month, day)
-
-# results = runIsolated(sql)
-# for route in results:
-# 	sql = "call dijaddpath('%s', '%s',  1);" % (route[0], route[1])
-# 	runIsolated(sql)
-
-# createDijkstraProcedure()
-
-# sql = "CALL dijResolve('%s','%s');" % (departure, arrival)
-# results = runIsolated(sql)
-
-# table = PrettyTable(["departure", "arrival", "route_part"])
-# for row in results:
-# 	table.add_row(row)
-
-# dropDijkstra()
-
-# b = datetime.datetime.now()
-# print table
-# print "Time: " + str(b-a) + "\n"
-
-
-# # Query 3.b
-# print "=========================================================================="
-# print "Query #3.b: Consider only high frequence routes (greater than the average)"
-# print "--------------------------------------------------------------------------"
-
-# year = "2011"
-# month = "6"
-# day = "20"
-# departure = "RAP"
-# arrival = "XNA"
-
-# print "Route: %s -> %s" % (departure, arrival)
-# print "Departure: %s/%s/%s" % (day, month, year)
-
-# a = datetime.datetime.now()
-
-# initDBforDijkstra()
-
-# sql = """
-# 	SELECT SQL_NO_CACHE r.origin, r.destination
-# 	FROM flights.routes r
-# 	WHERE r.year = "%s" AND r.month = "%s" AND r.day = "%s"
-# 	AND r.frequency > (
-# 		SELECT avg(r.frequency)
-# 		FROM flights.routes r
-# 		WHERE r.year = "%s" AND r.month = "%s" AND r.day = "%s"
-# 	);;
-# 	""" % (year, month, day, year, month, day)
-
-# results = runIsolated(sql)
-# for route in results:
-# 	sql = "call dijaddpath('%s', '%s',  1);" % (route[0], route[1])
-# 	runIsolated(sql)
-
-# createDijkstraProcedure()
-
-# sql = "CALL dijResolve('%s','%s');" % (departure, arrival)
-# results = runIsolated(sql)
-
-# table = PrettyTable(["departure", "arrival", "route_part"])
-# for row in results:
-# 	table.add_row(row)
-
-# dropDijkstra()
-
-# b = datetime.datetime.now()
-# print table
-# print "Time: " + str(b-a) + "\n"
+table = PrettyTable(["year", "airport", "tot_flights"])
+for row in results:
+	table.add_row(row)
+print table
+print map(str, timeList)
+print "Time: " + str(time) + "\n"
 
 
-# # Query 4
-# print "==============================================================="
-# print "Query #4: Find the state with more internal flights (per month)"
+# Query 3
+print "========================================================================================"
+print "Query #3: Given a departure date find all the shortest paths from airport A to airport B"
+print "----------------------------------------------------------------------------------------"
 
-# sql = """
-# 	SELECT SQL_NO_CACHE r1.year, r1.month, r1.state, max(r1.monthly_freq) as monthly_freq
-# 	FROM ( 
-# 		SELECT r.year, r.month, a1.state, sum(r.frequency) as monthly_freq
-# 		FROM flights.routes r
-# 		JOIN flights.airports a1 ON a1.code = r.origin
-# 		JOIN flights.airports a2 ON a2.code = r.destination
-# 		WHERE a1.state = a2.state
-# 		GROUP BY r.year, r.month, a1.state
-# 		ORDER BY sum(r.frequency) DESC ) as r1
-# 	GROUP BY r1.year, r1.month;
-# 	"""
-# (results, timeList, time) = runQueryAndGetTime(sql)
+year = "2011"
+month = "6"
+day = "20"
+departure = "RAP"
+arrival = "XNA"
 
-# table = PrettyTable(["year", "month", "state", "monthly_freq"])
-# for row in results:
-# 	table.add_row(row)
-# print table
-# print map(str, timeList)
-# print "Time: " + str(time) + "\n"
+print "Route: %s -> %s" % (departure, arrival)
+print "Departure: %s/%s/%s" % (day, month, year)
+
+a = datetime.datetime.now()
+
+initDBforDijkstra()
+
+sql = """
+	SELECT SQL_NO_CACHE r.origin, r.destination
+	FROM flights.routes r
+	WHERE r.year = "%s" AND r.month = "%s" AND r.day = "%s";
+	""" % (year, month, day)
+
+results = runIsolated(sql)
+for route in results:
+	sql = "call dijaddpath('%s', '%s',  1);" % (route[0], route[1])
+	runIsolated(sql)
+
+createDijkstraProcedure()
+
+sql = "CALL dijResolve('%s','%s');" % (departure, arrival)
+results = runIsolated(sql)
+
+table = PrettyTable(["departure", "arrival", "route_part"])
+for row in results:
+	table.add_row(row)
+
+dropDijkstra()
+
+b = datetime.datetime.now()
+print table
+print "Time: " + str(b-a) + "\n"
+
+
+# Query 3.b
+print "=========================================================================="
+print "Query #3.b: Consider only high frequence routes (greater than the average)"
+print "--------------------------------------------------------------------------"
+
+year = "2011"
+month = "6"
+day = "20"
+departure = "RAP"
+arrival = "XNA"
+
+print "Route: %s -> %s" % (departure, arrival)
+print "Departure: %s/%s/%s" % (day, month, year)
+
+a = datetime.datetime.now()
+
+initDBforDijkstra()
+
+sql = """
+	SELECT SQL_NO_CACHE r.origin, r.destination
+	FROM flights.routes r
+	WHERE r.year = "%s" AND r.month = "%s" AND r.day = "%s"
+	AND r.frequency > (
+		SELECT avg(r.frequency)
+		FROM flights.routes r
+		WHERE r.year = "%s" AND r.month = "%s" AND r.day = "%s"
+	);;
+	""" % (year, month, day, year, month, day)
+
+results = runIsolated(sql)
+for route in results:
+	sql = "call dijaddpath('%s', '%s',  1);" % (route[0], route[1])
+	runIsolated(sql)
+
+createDijkstraProcedure()
+
+sql = "CALL dijResolve('%s','%s');" % (departure, arrival)
+results = runIsolated(sql)
+
+table = PrettyTable(["departure", "arrival", "route_part"])
+for row in results:
+	table.add_row(row)
+
+dropDijkstra()
+
+b = datetime.datetime.now()
+print table
+print "Time: " + str(b-a) + "\n"
+
+
+# Query 4
+print "==============================================================="
+print "Query #4: Find the state with more internal flights (per month)"
+
+sql = """
+	SELECT SQL_NO_CACHE r1.year, r1.month, r1.state, max(r1.monthly_freq) as monthly_freq
+	FROM ( 
+		SELECT r.year, r.month, a1.state, sum(r.frequency) as monthly_freq
+		FROM flights.routes r
+		JOIN flights.airports a1 ON a1.code = r.origin
+		JOIN flights.airports a2 ON a2.code = r.destination
+		WHERE a1.state = a2.state
+		GROUP BY r.year, r.month, a1.state
+		ORDER BY sum(r.frequency) DESC ) as r1
+	GROUP BY r1.year, r1.month;
+	"""
+(results, timeList, time) = runQueryAndGetTime(sql)
+
+table = PrettyTable(["year", "month", "state", "monthly_freq"])
+for row in results:
+	table.add_row(row)
+print table
+print map(str, timeList)
+print "Time: " + str(time) + "\n"
 
 
 # Query 4.b
